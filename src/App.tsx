11f8edc4d472cde636a9dd7fcfebf855ddcd8c78
ajones-programming <ajones.programming@gmail.com>
2025-03-11 22:35:58 +0000

    test update

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,7 @@ function App() {
       </div>
       <h1>Vite + React</h1> */}
       <div className="card">
+        TEST
         <input value={result} onChange={e => setResult(e.target.value)}/>
         <button onClick={() => textToSpeech(result)}>TEXT TO SPEECH</button>
       </div>
