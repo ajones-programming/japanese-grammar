e9b63d2a3a448539837294d4e68a7e0034b8ca92
Anna Jones <ajones.programming@gmail.com>
2025-06-12 22:28:41 +0100

    only calls once per new word

--- src/TextToSpeech.ts
+++ src/TextToSpeech.ts
@@ -8,15 +8,21 @@
 //   audioConfig : {audioEncoding : 'MP3'}
 // }
 const address = "https://ajones-jp-text-to-speech-uk.azurewebsites.net/test";
+var retrieved : Iterable<number>;
+var previous = "";
 
 //https://stackoverflow.com/questions/59955096/how-to-decode-binary-audio-data
 export async function textToSpeech(toSet : string) {
 
-  const a = await window.fetch(address + "/" + toSet);
-
-
+  toSet = toSet.trim();
+  if (toSet != previous){
+    previous = toSet
+    const a = await window.fetch(address + "/" + toSet);
+    retrieved = (await a.json()).data.data;
+  }
+  const arr = Uint8Array.from(retrieved);
+  
   const audioContext = new AudioContext();
-  const arr = Uint8Array.from((await a.json()).data.data)
   const audio = await audioContext.decodeAudioData(arr.buffer);
   const source = audioContext.createBufferSource();
   source.buffer = audio;
